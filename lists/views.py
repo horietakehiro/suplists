1fea4284a8172a3fec99374aafd086aee8dd648c
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-02-10 12:08:38 +0900

    add validation UT

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
+
 from lists.models import Item, List
 
 # Create your views here.
@@ -18,7 +20,16 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    # item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {'error' : error})
+
     return redirect(f'/lists/{list_.id}/')
 
     
