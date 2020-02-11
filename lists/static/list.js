da3e51b77af115d0e5c4a95804e053ce483a1a74
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-02-11 12:34:01 +0900

    add js

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,5 @@
-const initialize = function () {
+window.Superlists = {};
+window.Superlists.initialize = function () {
     console.log('initialize called');
     $("input[name='text']").on('keypress', function () {
         $('.has-error').hide();
