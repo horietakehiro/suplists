a77f0c6aa569f2e106dac97b82639beb8764dc9c
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-02-11 12:12:10 +0900

    add jquery test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -20,6 +20,9 @@ from .base import FunctionalTest
 MAX_WAIT=10
 
 class ItemValidationTest(FunctionalTest):
+
+    def get_error_element(self):
+        return self.driver.find_element_by_css_selector('.has-error')
     
     def test_cannnot_add_empty_list_items(self):
         # Edith goes the home page and accidentlly tries to submit 
@@ -77,8 +80,29 @@ class ItemValidationTest(FunctionalTest):
 
         # she sees a helpful error message
         self.wait_for(lambda : self.assertEqual(
-            self.driver.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already get this in your list"
         ))
 
+
+    def test_error_messages_are_cleared_on_input(self):
+        #Edith starts a list and cause a validation error
+        self.driver.get(self.my_live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_rable('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda : self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+        
+        #She is pleased to see that the error message disappears
+        self.wait_for(lambda : self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
 
