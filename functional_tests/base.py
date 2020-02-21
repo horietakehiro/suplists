e37dd372a024d70981bf1f1593fc0d655f9681c2
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-02-21 20:13:23 +0900

    creating my_lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -86,3 +86,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         navbar = self.driver.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
         
+
+    def add_list_item(self, item_text):
+        num_rows = len(self.driver.find_elements_by_css_selector('#id_list_table tr'))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_rable(f'{item_number}: {item_text}')
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -32,3 +32,48 @@ class MylistsTest(FunctionalTest):
         self.driver.get(self.my_live_server_url)
         self.wait_to_be_logged_in(email=email)
         
+
+    def test_logged_in_users_lists_are_saevd_as_my_lists(self):
+        #Edith is a logged-in user
+        self.create_pre_authenticated_session('edith@example.com')
+        
+        # She goes to the home page and starts a list
+        self.driver.get(self.my_live_server_url)
+        self.add_list_item('Reticulate splines')
+        self.add_list_item('Immanentize eschaton')
+        first_list_url = self.driver.current_url
+
+        #she notices a "My lists" lnik, for the first time
+        self.driver.find_element_by_link_text('My lists').click()
+
+        # She sees that her list is in there, named according to its
+        # first list item
+        self.wait_for(
+            lambda : self.driver.find_element_by_link_text('Reticulate splines')
+        )
+        self.driver.find_element_by_link_text('Reticulate splines').click()
+        self.wait_for(
+            lambda : self.assertEqual(self.driver.current_url, first_list_url)
+        )
+
+        # she decides to start another list, just to see
+        self.driver.get(self.my_live_server_url)
+        self.add_list_item('Click cows')
+        second_url = self.driver.current_url
+
+        # Under "my lsits", her new list appears
+        self.driver.find_element_by_link_text('My lists').click()
+        self.wait_for(
+            lambda : self.driver.find_element_by_link_text('Click cows')
+        )
+        self.driver.find_element_by_link_text('Click cows').click()
+        self.wait_for(
+            lambda : self.assertEqual(self.driver.current_url, second_url)
+        )
+
+        #she logged out. The "my lists" option disappears
+        self.driver.find_element_by_link_text('Log out').click()
+        self.wait_for(lambda : self.assertEqual(
+            self.driver.find_elements_by_link_text('My lists'), []
+            )
+        )
